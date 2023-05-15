9aa5eedd7f8c6a3127eae3fc1e8cb4f87974799f
Laura Lieknina <laura@magicworks.lv>
2023-05-15 22:12:47 +0300

    change change detection strategy to OnPush and call custom change detection

--- src/app/timer/timer.component.ts
+++ src/app/timer/timer.component.ts
@@ -1,29 +1,47 @@
-import { Component, OnInit, OnDestroy, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, Output, EventEmitter, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { TimerService } from './timer.service';
 
 @Component({
   selector: 'app-timer',
   templateUrl: './timer.component.html',
   styleUrls: ['./timer.component.scss'],
-  providers: [TimerService]
+  providers: [TimerService],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class TimerComponent implements OnInit, OnDestroy {
 
   @Output() onComplete = new EventEmitter<void>();
   @Input() init: number = 20;
+  private countdownSubscription!: Subscription;
+  private countdownEndSubscription!: Subscription;
+  public countdown: number = 0;
 
-  constructor(public timer: TimerService) { }
+  //getter
+  get progress () {
+    console.log('progress');
+    return (this.init - this.countdown) / this.init * 100;
+  }
+
+  constructor(public timer: TimerService, private cdRef: ChangeDetectorRef) { }
 
   ngOnInit (): void {
     this.timer.restartCountdown(this.init);
 
-    this.timer.countdownEnd$.subscribe(() => {
-      console.log('---countdown end');
+    this.countdownEndSubscription = this.timer.countdownEnd$.subscribe(() => {
       this.onComplete.emit();
+    });
+
+    this.countdownSubscription = this.timer.countdown$.subscribe((data) => {
+      this.countdown = data;
+      this.cdRef.markForCheck();
     });
   };
 
   ngOnDestroy (): void {
     this.timer.destroy();
+    this.countdownSubscription.unsubscribe();
+    this.countdownSubscription.unsubscribe();
   }
 }
